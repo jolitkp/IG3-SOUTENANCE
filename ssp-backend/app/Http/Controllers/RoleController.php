03faa042c1d2d3f604ccd0e5c688714e174bc007
jolitkp <tokpanoujoliana@gmail.com>
2023-06-21 11:18:05 +0100

    LARAVELNEW

--- ssp-backend/app/Http/Controllers/RoleController.php
+++ ssp-backend/app/Http/Controllers/RoleController.php
@@ -16,6 +16,8 @@ class RoleController extends Controller
         'roleName' => 'required|string|max:255|unique:roles,name',
         'selectedPermissions' => 'array',
         'selectedPermissions.*' => 'string|exists:permissions,id',
+    ],[
+        'roleName.unique' => 'Le nom du rôle est déjà pris.',
     ]);
 
 
@@ -23,26 +25,33 @@ class RoleController extends Controller
         return response()->json(['errors'=>$validator->errors()], 422);
     }
 
+
+    //recupérer les permissions sélectionnées
+
     $selectedPermissions = $request->input('selectedPermissions');
     //$permission = Permission::whereIn('name',$validatedData['permissions'])->get();
 
 
     // créer le role
+
     //$role= new Role();
     $role = Role::create(['name'=>$request->input('roleName')]);
 
-    //recupérer les permissions sélectionnées
 
 
     //verifier et assigner les permissions au role
+    if ($selectedPermissions !== null){
+    foreach( $selectedPermissions as $permissionName){
+        $permissions = Permission::findByName($permissionName);
+        $role->syncPermissions( $permissions);
+        // $role->syncPermissions($selectedPermissions);
 
-    /*foreach($permissions as $permissionName){
-        $permission = Permission::findByName($permissionName);
-        $role->givePermissionTo($permission);
-    }*/
-
-    $role->syncPermissions($selectedPermissions);
+    }
+    }
+    
+    // Enregistrer le rôle
     $role->save();
+   
 
 
     return response()->json(['message' => 'Role créé avec succès']);
