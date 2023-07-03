55abddc085a54856742d14ef5423db048816298f
Patoute <amoussoupatou@gmail.com>
2023-07-03 11:03:23 +0100

    login, env variable

--- Frontendangular/src/app/components/login/login.component.ts
+++ Frontendangular/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 //import { AuthService } from '../../services/auth.service';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
@@ -24,7 +25,14 @@ export class LoginComponent {
 
 
   onSubmit() {
-     return this.http.post('http://127.0.0.1:8000/api/login-page', this.form).subscribe(
+    let data = {
+      "grant_type":"password",
+      "client_id":2,
+      "client_secret":"1bldDpV9wTwSjQrzyQNKJTV1oXLobVFgQIHYiXZR",
+      "username":this.form.email,
+      "password":this.form.password
+    };
+     return this.http.post(environment.publicUrl+'oauth/token', data).subscribe(
 
       (res:any)=>{
         //console.log(res),
---
+++ Frontendangular/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+
+  /** Production */
+  baseUrl: 'http://127.0.0.1:8000/api',
+  publicUrl: 'http://127.0.0.1:8000/',
+
+};
